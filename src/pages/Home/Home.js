d29f251a2770afc5d58bbf3bd04652624c784540
Bappy-Das <bappydas.bd51@gmail.com>
2022-03-31 17:04:13 +0600

    added course section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,15 @@ import Reviews from '../Home/Reviews/Reviews';
 import JoinCommunity from './JoinCommunity/JoinCommunity';
 import TopCategory from './TopCategory/TopCategory';
 import Instructor from "./Instructor/Instructor";
+import Courses from './Courses/Courses';
 const Home = () => {
     return (
         <div>
             <TopCategory />
+            <Courses />
             <Reviews />
             <Instructor />
-            <JoinCommunity></JoinCommunity>
+            <JoinCommunity />
         </div>
     );
 
--- src/pages/Home/TopCategory/TopCategory.js
+++ src/pages/Home/TopCategory/TopCategory.js
@@ -57,12 +57,12 @@ const courses = [
 
 const TopCategory = () => {
     return (
-        <section className="py-10">
+        <section className="container py-10">
             <div className='px-4 py-10'>
                 <h1 className='text-center mx-auto font-Roboto font-bold text-3xl lg:text-4xl '><span className="text-indigo-500 ">Explore</span> <br />
-                    Our Top Trendy
+                    Our Top Trendy Course
                     <span className='relative ml-2'>
-                        Course
+                        Category
                         <div className='absolute animate-bounce -bottom-4 left-0 w-20 h-4'>
                             <div className='w-16 h-2 animate-pulse  rounded-tr-full bg-red-500'></div>
                         </div>
@@ -70,7 +70,7 @@ const TopCategory = () => {
                 </h1>
 
             </div>
-            <div className='container mx-auto '>
+            <div className=' mx-auto '>
 
                 <Swiper
                     slidesPerView={5}
