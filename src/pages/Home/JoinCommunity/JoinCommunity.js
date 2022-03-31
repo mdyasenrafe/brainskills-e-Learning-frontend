636adcb7aedc48188bbe12e7c39c0ffc0c6b0339
Bappy-Das <bappydas.bd51@gmail.com>
2022-03-31 08:55:35 +0600

    update join community

--- src/pages/Home/JoinCommunity/JoinCommunity.js
+++ src/pages/Home/JoinCommunity/JoinCommunity.js
@@ -9,19 +9,19 @@ const JoinCommunity = () => {
                     <div class="md:shrink-0">
                         <img class="" src="https://i.ibb.co/3m97DKW/education.png" alt="Man looking at item at a store" />
                     </div>
-                    <div class="p-8">
+                    <div class="p-10">
                         <h1 class=" tracking-wide font-bold text-4xl text-indigo-500 font-semibold">Transform Your Life Through Education</h1>
 
 
-                        <p class="mt-2 mb-2 text-slate-500">Helping employees gain skills and providing career development often take a back seat to business priorities but workplace.</p>
+                        <p class="mt-8 mb-2 text-slate-500">Helping employees gain skills and providing career development often take a back seat to business priorities but workplace.</p>
 
                         <h1 class=" tracking-wide font-bold text-2xl text-indigo-500 font-semibold">Join Our Community</h1>
 
-                        <div>
-                            <form class="mt-2 flex">
+                        <div className='mt-8'>
+                            <form class=" flex">
                                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="your@mail.com" />
 
-                                <button class="px-8 rounded-r-lg bg-indigo-500  font-bold p-4 uppercase border-indigo-500 border-t border-b border-r">Subscribe</button>
+                                <button class="px-8 rounded-r-lg bg-indigo-500  font-bold p-4 text-white uppercase border-indigo-500 border-t border-b border-r">Subscribe</button>
                             </form>
                         </div>
                     </div>
