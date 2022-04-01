cd9f6e799371a99ab1d7c21c2492325509d7feab
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-02 02:32:47 +0600

    container added

--- src/pages/Home/Navbar/Navbar.js
+++ src/pages/Home/Navbar/Navbar.js
@@ -1,37 +1,24 @@
-import React, { useEffect } from 'react';
-import './Navbar.css';
+import React, { useEffect } from "react";
+import "./Navbar.css";
 import nablog from "../../../images/navber.1.png";
-import TopNavbar from '../../Shared/TopNavbar/TopNavbar';
-
+import TopNavbar from "../../Shared/TopNavbar/TopNavbar";
 
 const Navbar = () => {
   useEffect(() => {
-    const button = document.querySelector('#menu-button');
-    const menu = document.querySelector('#menu');
+    const button = document.querySelector("#menu-button");
+    const menu = document.querySelector("#menu");
 
-
-    button.addEventListener('click', () => {
-      menu.classList.toggle('hidden');
+    button.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
     });
-
-  }, [])
+  }, []);
   return (
     <div>
       <TopNavbar></TopNavbar>
       <header>
         <nav
           class="
-          flex flex-wrap
-          items-center
-          justify-between
-          w-full
-          py-4
-          md:py-0
-          px-4
-          text-lg text-gray-700
-          bg-white
-          h-24
-        "
+          flex flex-wrap  items-center  justify-between  w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white h-24 container mx-auto"
         >
           <div>
             <a href="#">
@@ -55,7 +42,10 @@ const Navbar = () => {
             />
           </svg>
 
-          <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
+          <div
+            class="hidden w-full md:flex md:items-center md:w-auto"
+            id="menu"
+          >
             <ul
               class="
               pt-4
@@ -65,35 +55,40 @@ const Navbar = () => {
               md:pt-0"
             >
               <li>
-                <a class="md:p-4 py-2 block hover:text-purple-400" href="/" >Features</a>
+                <a class="md:p-4 py-2 block hover:text-purple-400" href="/">
+                  Features
+                </a>
               </li>
               <li>
-                <a class="md:p-4 py-2 block hover:text-purple-400" href="#"
-                >Pricing</a
+                <a class="md:p-4 py-2 block hover:text-purple-400" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a class="md:p-4 py-2 block hover:text-purple-400" href="#">
+                  Customers
+                </a>
+              </li>
+              <li>
+                <a
+                  class="md:p-4 py-2 block hover:text-purple-400"
+                  href="/aboutus"
                 >
-              </li>
-              <li>
-                <a class="md:p-4 py-2 block hover:text-purple-400" href="#"
-                >Customers</a
-                >
-              </li>
-              <li>
-                <a class="md:p-4 py-2 block hover:text-purple-400" href="/aboutus"
-                >About us</a>
+                  About us
+                </a>
               </li>
               <li>
                 <a
                   class="md:p-4 py-2 block hover:text-purple-400 text-purple-500"
                   href="#"
-                >Sign Up</a
                 >
+                  Sign Up
+                </a>
               </li>
             </ul>
           </div>
         </nav>
       </header>
-
-
     </div>
   );
 };
