c2a106550571b774f5b8da4241dddd2e4725af79
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2022-04-01 20:41:07 +0600

    in-card

--- src/pages/Home/Instructor/Instructor.js
+++ src/pages/Home/Instructor/Instructor.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getInstructor } from "../../../API";
 import { Swiper, SwiperSlide } from "swiper/react";
-import img from "../../../images/swiper.jpg";
-import img2 from "../../../images/swiper2.jpg";
-import img3 from "../../../images/swiper3.jpg";
-import img4 from "../../../images/swiper4.jpg";
 
 // Import Swiper styles
 import "swiper/css";
@@ -12,44 +8,12 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import {Autoplay,Pagination} from "swiper";
+import { Autoplay, Pagination } from "swiper";
 
 const Instructor = () => {
-  const swiperdata = [
-    {
-      img: img,
-      name: "abc",
-      propesion: "developer"
-    },
-    {
-      img: img2,
-      name: "abc",
-      propesion: "developer"
-    },
-    {
-      img: img2,
-      name: "abc",
-      propesion: "developer"
-    },
-    {
-      img: img2,
-      name: "abc",
-      propesion: "developer"
-    },
-    {
-      img: img2,
-      name: "abc",
-      propesion: "developer"
-    },
-    {
-      img: img3,
-      name: "abc",
-      propesion: "developer"
-    }
-    
-  ]
-  
-  
+
+
+
   const [instructors, setInstructors] = useState();
 
   useEffect(() => {
@@ -70,7 +34,7 @@ const Instructor = () => {
   return (
     <>
       <section className="container mx-auto py-24">
-        <div className="mx-6 grid lg:grid-cols-2 gap-7 spac-y-4">
+        <div className="mx-6 grid grid-cols-1 lg:grid-cols-2 gap-7 spac-y-4">
           <div>
             <h1 className="text-black mb-4 font-bold text-3xl">
               These Are Our{" "}
@@ -107,19 +71,30 @@ const Instructor = () => {
                 delay: 2500,
                 disableOnInteraction: false,
               }}
-             
+              breakpoints={{
+                "@0.00": {
+                  slidesPerView: 1,
+
+                },
+                "@1.50": {
+                  slidesPerView: 2,
+
+                },
+                "@1.75": {
+                  slidesPerView: 3,
+
+                }
+              }}
               modules={[Pagination, Autoplay]}
               className="mySwiper"
             >
               {
                 instructors?.map(data => <SwiperSlide className="py-10">
-                  <div style={{ width: "150px" }}>
-                    <img style={{ height: "150px", width: "100%" }} src={data.instructorPhoto} alt="" srcset="" />
+                  <div className="text-center bg-gray-50 p-3 rounded-md shadow-md">
+                    <img className="h-48 w-48 rounded-lg mx-auto" src={data.instructorPhoto} alt="" srcset="" />
+                    <h1 className="text-lg  font-semibold">{data.instructorName}</h1>
+                    <h1>{data.instructorDesignation}</h1>
                   </div>
-                  <h1>{data.instructorName}</h1>
-                  <h1>{data.instructorDesignation}</h1>
-
-
                 </SwiperSlide>)
               }
 
