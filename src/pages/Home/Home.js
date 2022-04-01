a613c17eea9cc97e90d16ad2d037996bfb3b811d
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-02 02:18:35 +0600

    feature section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import Reviews from '../Home/Reviews/Reviews';
-import JoinCommunity from './JoinCommunity/JoinCommunity';
-import TopCategory from './TopCategory/TopCategory';
+import React from "react";
+import Reviews from "../Home/Reviews/Reviews";
+import JoinCommunity from "./JoinCommunity/JoinCommunity";
+import TopCategory from "./TopCategory/TopCategory";
 import Instructor from "./Instructor/Instructor";
-import Courses from './Courses/Courses';
-import Blogs from './Blogs/Blogs';
-import Events from './Events/Events';
+import Courses from "./Courses/Courses";
+import Blogs from "./Blogs/Blogs";
+import Events from "./Events/Events";
 
-import Navbar from './Navbar/Navbar';
-import TopHader from '../Shared/TopHader/TopHader';
-import { ScrollTop } from '../Scroll/Scroll';
-import Footer from '../Shared/Footer/Footer';
+import Navbar from "./Navbar/Navbar";
+import TopHader from "../Shared/TopHader/TopHader";
+import { ScrollTop } from "../Scroll/Scroll";
+import Footer from "../Shared/Footer/Footer";
+import Feature from "./Feature/Feature";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <TopHader></TopHader>
-            <TopCategory />
-            <Courses />
-            <Reviews />
-            <Instructor />
-            <Blogs />
-            <Events />
-            <JoinCommunity />
-            <ScrollTop></ScrollTop>
-            <Footer />
-        </div>
-    );
-
+  return (
+    <div>
+      <Navbar></Navbar>
+      <TopHader></TopHader>
+      <Feature />
+      <TopCategory />
+      <Courses />
+      <Reviews />
+      <Instructor />
+      <Blogs />
+      <Events />
+      <JoinCommunity />
+      <ScrollTop></ScrollTop>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
