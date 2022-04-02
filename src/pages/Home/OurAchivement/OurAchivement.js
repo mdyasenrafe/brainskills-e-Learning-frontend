f98858bd090cd635edf66acdf52f7380404243ce
Bappy-Das <bappydas.bd51@gmail.com>
2022-04-02 10:52:30 +0600

    added achiver counter

--- src/pages/Home/OurAchivement/OurAchivement.js
+++ src/pages/Home/OurAchivement/OurAchivement.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const OurAchivement = () => {
+
+
     return (
 
         <section className=" py-10">
@@ -36,7 +40,17 @@ const OurAchivement = () => {
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mx-16 ">
                     <div className="flex flex-col justify-center items-center h-48 text-indigo-500  shadow-md rounded-md bg-slate-50">
                         <div>
-                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>25,000+</h1>
+                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>
+
+                                <CountUp end={2500} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                                +
+                            </h1>
                         </div>
                         <div >
                             <h2>Courses & Videos</h2>
@@ -44,7 +58,17 @@ const OurAchivement = () => {
                     </div>
                     <div className="flex flex-col justify-center items-center h-48 text-indigo-500  shadow-md rounded-md bg-slate-50">
                         <div>
-                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>25,000+</h1>
+                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>
+
+                                <CountUp end={30000} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                                +
+                            </h1>
                         </div>
                         <div >
                             <h2>Students Enrolled</h2>
@@ -52,7 +76,17 @@ const OurAchivement = () => {
                     </div>
                     <div className="flex flex-col justify-center items-center h-48 text-indigo-500  shadow-md rounded-md bg-slate-50">
                         <div>
-                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>25,000+</h1>
+                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>
+
+                                <CountUp end={100} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                                +
+                            </h1>
                         </div>
                         <div >
                             <h2>Courses Instructors</h2>
@@ -60,7 +94,17 @@ const OurAchivement = () => {
                     </div>
                     <div className="flex flex-col justify-center items-center h-48 text-indigo-500  shadow-md rounded-md bg-slate-50">
                         <div>
-                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>25,000+</h1>
+                            <h1 className='mx-auto font-Roboto font-bold text-3xl lg:text-4xl my-1'>
+
+                                <CountUp end={45000} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                            <span ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                                +
+                            </h1>
                         </div>
                         <div >
                             <h2>Satisfaction Rate</h2>
