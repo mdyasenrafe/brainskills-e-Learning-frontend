863c5d6eca05aec8c75da3c3779f312954263a02
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2022-04-02 12:53:06 +0600

    error

--- src/pages/Home/Events/Events.js
+++ src/pages/Home/Events/Events.js
@@ -16,8 +16,8 @@ const Events = () => {
         }
     }
     return (
-        <div>
-            <div className="container mx-auto px-4 py-10">
+        <div className=''>
+            <div className="container  mx-auto px-4 py-10">
                 <h1 className='text-4xl font-bold text-center mb-10'>Upcoming Events Schedule</h1>
                 {/* box  */}
                 <div className='grid grid-cols-3 gap-4'>
