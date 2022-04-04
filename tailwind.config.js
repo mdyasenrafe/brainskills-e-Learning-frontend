11e7672453ea64f14f9152b84c0793aacf2f73d8
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2022-04-04 22:54:54 +0600

    not found

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         'Poppins': ['Poppins', "sans-serif"],
         'Roboto': ['Roboto', "sans-serif"],
       },
+      fontSize: {
+        '12xl': '14rem',
+        '13xl': '16rem',
+        '14xl': '18rem',
+        '15xl': '20rem',
+      }
     },
   },
   plugins: [],
