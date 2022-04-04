27d3e1b529ff401bd5df37887be1e61383868c6e
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2022-04-04 13:54:54 +0600

    course grid

--- src/pages/Home/Courses/Courses/Courses.js
+++ src/pages/Home/Courses/Courses/Courses.js
@@ -33,7 +33,7 @@ const Courses = () => {
       </div >
       {/*  */}
       <div className="container mx-auto p-16">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-auto ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-auto ">
           <Course />
           <Course />
           <Course />
