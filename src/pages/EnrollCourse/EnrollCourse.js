43c96f7abfbf654654bbada8a134ca6a15e82c37
Bappy-Das <bappydas.bd51@gmail.com>
2022-04-05 15:09:49 +0600

    complete enroll course

--- src/pages/EnrollCourse/EnrollCourse.js
+++ src/pages/EnrollCourse/EnrollCourse.js
@@ -8,13 +8,26 @@ const EnrollCourse = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto ">
 
                         <div className="shadow-md rounded-md border-b-4 hover:border-b-blue-400 border-b-blue-300 bg-slate-100 rounded-t-lg overflow-hidden">
-                            <div className='flex justify-center item-center'>
-                                <div className='w-60 h-auto'>
+
+                            <div className="grid grid-cols-2 mx-auto ">
+
+
+                                <div className='overflow-hidden p-1'>
                                     <img src="https://i.ibb.co/PxFbNX3/course-11-b10ef53de8.jpg" alt="" srcset="" />
                                 </div>
-                                <div className=''>
-                                    <h1 className='text-2xl'>Web Development</h1>
-                                    <h1>Web Development</h1>
+                                <div>
+                                    <div className='p-2'>
+                                        <h1 className='text-xl'>Lua Programming and Game Development with LOVE in 2021</h1>
+                                        <div className='my-2'>
+                                            <h1>By : Sullivan</h1>
+                                            <h1>Web Development</h1>
+                                        </div>
+                                    </div>
+
+                                    <div className='flex justify-end m-2'>
+                                        <button className="bg-indigo-500 rounded-sm border-indigo-500 p-2 hover:bg-indigo-600 text-white hover:rounded-md ease-in duration-300">Continue Course</button>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
