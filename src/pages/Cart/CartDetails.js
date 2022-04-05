0b889d80f8652e8f3fe6acc01e1911bd39ad37f6
musasazibe <musasazib@gmail.com>
2022-04-05 21:38:02 +0600

    Add cart course tablek

--- src/pages/Cart/CartDetails.js
+++ src/pages/Cart/CartDetails.js
@@ -2,33 +2,33 @@ import React from 'react';
 
 const CartDetails = () => {
     return (
-        <div className=''>
+        <div className="container">
             <h1 className='text-4xl	font-bold text-center my-16'>Cart</h1>
-            <div className="mx-6 grid grid-cols-1 lg:grid-cols-2 gap-7 spac-y-4">
+            <div className="mx-10 grid grid-cols-1 lg:grid-cols-2 gap-7 mb-28">
                 <div>
                     <table className="border-collapse border border-slate-400">
                         <thead>
                             <tr>
-                                <th className="border border-slate-300 font-medium py-3	px-6 text-left">Courses</th>
-                                <th className="border border-slate-300 font-medium py-3	px-6 text-left">Price</th>
-                                <th className="border border-slate-300 font-medium py-3	px-6 text-left"></th>
+                                <th className="border border-slate-300 font-medium py-3	px-6 text-left text-base">Courses</th>
+                                <th className="border border-slate-300 font-medium py-3	px-6 text-left text-base">Price</th>
+                                <th className="border border-slate-300 font-medium py-3	px-6 text-left text-base"></th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td className="border border-slate-300">Practical Web Development: 22 Courses in 1 with practical examples</td>
-                                <td className="border border-slate-300">$11.00</td>
-                                <td className="border border-slate-300">**</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">Practical Web Development: 22 Courses in 1 with practical examples</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">$11.00</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">**</td>
                             </tr>
                             <tr>
-                                <td className="border border-slate-300">Lua Programming and Game Development with LOVE in 2021</td>
-                                <td className="border border-slate-300">$89.00</td>
-                                <td className="border border-slate-300">**</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">Lua Programming and Game Development with LOVE in 2021</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">$89.00</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">**</td>
                             </tr>
                             <tr>
-                                <td className="border border-slate-300">Make a 2D Platformer Character with State Machines in Unity</td>
-                                <td className="border border-slate-300">$49.00</td>
-                                <td className="border border-slate-300">**</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">Make a 2D Platformed Character with State Machines in Unity</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">$49.00</td>
+                                <td className="border border-slate-300 py-4	px-6 font-medium text-sm">**</td>
                             </tr>
                         </tbody>
                     </table>
