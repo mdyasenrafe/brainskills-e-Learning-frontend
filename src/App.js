267f6106989b88b39bc35b5ebe9e702ecb8bdfd2
mdyasenrafe <mdyasenrafe@gmail.com>
2022-04-11 16:09:51 +0600

    done

--- src/App.js
+++ src/App.js
@@ -28,8 +28,11 @@ import CreateAdmin from "./pages/UserDashboard/CreateAdmin/CreateAdmin";
 import AddNewCourse from "./pages/UserDashboard/AddNewCourse/AddNewCourse";
 import Bookmark from "./pages/Bookmark/Bookmark";
 import CourseQuiz from "./pages/CourseQuiz/CourseQuiz";
+import GetUser from "./hooks/GetUser";
+import LoadingSpiners from "./Componets/LoadingSpiners";
 
 function App() {
+  const { loading } = GetUser();
   const location = useLocation();
   const pn = location.pathname;
   let pathName = pn?.substring(1);
@@ -41,52 +44,59 @@ function App() {
       document.title = pathName;
     }
   }, [location]);
-
+  console.log(loading);
   return (
     <div>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/payment/failure/:id" element={<Process />} />
-        <Route path="/payment/success/:id" element={<Sucess />} />
-        <Route path="/payment/processing/:id" element={<Failed />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/courseDetails/:id" element={<CourseDetails />} />
-        <Route path="/cart" element={<CartDetails />} />
-        <Route path="/course" element={<Courses />}></Route>
-        <Route path="/quiz" element={<CourseQuiz />}></Route>
-        <Route>
-          <Route path="/bookmark" element={<Bookmark />} />
-        </Route>
-        {/* course details*/}
-        <Route path="/courseId" element={<CourseDetails />}>
-          <Route path="overview" element={<Overview />} />
-          <Route path="curriculum" element={<Curriculum />} />
-          <Route path="instructor" element={<Instructor />} />
-          <Route path="reviews" element={<ReviewsCourse />} />
-          <Route path="" element={<Overview />} />
-        </Route>
-        {/* user dashboard  */}
-        <Route path="/userDashboard" element={<UserDashboard />}>
-          <Route path="profile" element={<UserProfile />} />
-          <Route path="address" element={<UserAddress />} />
-          <Route path="education" element={<UserEducation />} />
-          <Route path="orderHistory" element={<UserOrderHistory />} />
-          <Route path="certificate" element={<UserCertificate />} />
-          <Route path="createAdmin" element={<CreateAdmin />} />
-          <Route path="addNewCourse" element={<AddNewCourse />} />
-          <Route path="" element={<UserProfile />} />
-        </Route>
-        {/*  */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/enrollcourse" element={<EnrollCourse />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+      {loading ? (
+        <LoadingSpiners loading={loading} height={"100vh"} />
+      ) : (
+        <>
+          {" "}
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/payment/failure/:id" element={<Process />} />
+            <Route path="/payment/success/:id" element={<Sucess />} />
+            <Route path="/payment/processing/:id" element={<Failed />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/courseDetails/:id" element={<CourseDetails />} />
+            <Route path="/cart" element={<CartDetails />} />
+            <Route path="/course" element={<Courses />}></Route>
+            <Route path="/quiz" element={<CourseQuiz />}></Route>
+            <Route>
+              <Route path="/bookmark" element={<Bookmark />} />
+            </Route>
+            {/* course details*/}
+            <Route path="/courseDetails/:id" element={<CourseDetails />}>
+              <Route path="overview" element={<Overview />} />
+              <Route path="curriculum" element={<Curriculum />} />
+              <Route path="instructor" element={<Instructor />} />
+              <Route path="reviews" element={<ReviewsCourse />} />
+              <Route path="" element={<Overview />} />
+            </Route>
+            {/* user dashboard  */}
+            <Route path="/userDashboard" element={<UserDashboard />}>
+              <Route path="profile" element={<UserProfile />} />
+              <Route path="address" element={<UserAddress />} />
+              <Route path="education" element={<UserEducation />} />
+              <Route path="orderHistory" element={<UserOrderHistory />} />
+              <Route path="certificate" element={<UserCertificate />} />
+              <Route path="createAdmin" element={<CreateAdmin />} />
+              <Route path="addNewCourse" element={<AddNewCourse />} />
+              <Route path="" element={<UserProfile />} />
+            </Route>
+            {/*  */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/enrollcourse" element={<EnrollCourse />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
