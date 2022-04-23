aac0ef4a0312195d109b216bca361f44495f101e
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2022-04-23 23:35:10 +0600

    next

--- src/pages/Classes/Classes.js
+++ src/pages/Classes/Classes.js
@@ -1,110 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Classes.css'
 import ReactPlayer from 'react-player/lazy';
 import { FiBookmark, FiAlertTriangle } from "react-icons/fi";
 
-const url = "https://youtu.be/Ke90Tje7VS0";
-// dropdown list 
-const qna = [
+const course = [
     {
         id: 1,
         name: "Welcome to React",
+        video: "https://youtu.be/Ke90Tje7VS0"
 
     },
     {
         id: 2,
-        name: "Welcome to React",
+        name: "React 2",
+        video: "https://youtu.be/1NWBO8L81J8"
 
     },
     {
         id: 3,
-        name: "Welcome to React",
+        name: "React 3",
+        video: "https://youtu.be/SccSCuHhOw0"
 
     },
     {
         id: 4,
-        name: "Welcome to React",
+        name: "React 4",
+        video: "https://youtu.be/Ke90Tje7VS0"
     },
     {
-        id: 1,
-        name: "Welcome to React",
+        id: 5,
+        name: "React 5",
+        video: "https://youtu.be/Ke90Tje7VS0"
 
     },
     {
-        id: 2,
-        name: "Welcome to React",
+        id: 6,
+        name: "React 6",
+        video: "https://youtu.be/Ke90Tje7VS0"
 
-    },
-    {
-        id: 3,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 4,
-        name: "Welcome to React",
-    },
-    {
-        id: 1,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 2,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 3,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 4,
-        name: "Welcome to React",
-    },
-    {
-        id: 1,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 2,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 3,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 4,
-        name: "Welcome to React",
-    },
-    {
-        id: 1,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 2,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 3,
-        name: "Welcome to React",
-
-    },
-    {
-        id: 4,
-        name: "Welcome to React",
-    },
+    }
 ]
 
 const Classes = () => {
 
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const videoChangeHandler = (type) => {
+        switch (type) {
+            case "increase":
+                if (currentIndex === course.length - 1) return;
+                setCurrentIndex(currentIndex + 1)
+                break;
+
+            case "decrease":
+                if (currentIndex === 0) return;
+                setCurrentIndex(currentIndex - 1)
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    
     return (
         <div className='bg-white'>
             <div className='container mx-auto pt-5 px-4  '>
@@ -113,9 +71,12 @@ const Classes = () => {
                     {/* recorder  */}
                     <div className="w-full h-fit lg:w-8/12 overflow-hidden rounded-md ">
                         <div className='relative'>
+
+
+
                             <ReactPlayer
                                 className="react-player"
-                                url={url}
+                                url={course[currentIndex].video}
                                 config={{
                                     youtube: {
                                         playerVars: { showinfo: 1 }
@@ -134,15 +95,18 @@ const Classes = () => {
                                 controls={true}
                                 light={true}
                             />
+
+
+
                             <div className='absolute right-0 -bottom-9  py-2 px-5 bg-indigo-900 w-full text-white'>
                                 <button className='flex items-center float-right'>Bookmark <FiBookmark className='ml-2' /> </button>
                             </div>
                         </div>
                         <div className='pb-4 pt-14 px-2 flex justify-between items-center'>
-                            <h2 className='text-2xl '>What is react js?</h2>
+                            <h2 className='text-2xl '>{course[currentIndex].name}</h2>
                             <div className='space-x-4'>
-                                <button className='py-2 px-6 md:px-10 border-blue-600 border-2 rounded-full hover:bg-blue-600 hover:text-white ease-out duration-300'>Previous</button>
-                                <button className='py-2 px-6 md:px-10 border-blue-600 border-2 rounded-full bg-blue-600 text-white'>Next</button>
+                                <button onClick={() => videoChangeHandler("decrease")} className='py-2 px-6 md:px-10 border-blue-600 border-2 rounded-full hover:bg-blue-600 hover:text-white ease-out duration-300'>Previous</button>
+                                <button onClick={() => videoChangeHandler("increase")} className='py-2 px-6 md:px-10 border-blue-600 border-2 rounded-full bg-blue-600 text-white'>Next</button>
                             </div>
                         </div>
                         <div className='px-4 pb-5'>
@@ -157,9 +121,9 @@ const Classes = () => {
                             </div>
                             <div className='course-content-list rounded-lg '>
                                 {
-                                    qna.map(item => (
+                                    course.map((item, index) => (
                                         <div key={item?.id}>
-                                            <button className='w-full text-left p-2 py-3 mb-2 shadow-sm rounded-lg font-semibold bg-white border border-blue-100 text-gray-800 hover:shadow-md hover:border-emerald-400 ease-in-out duration-300 '>{item.name}</button>
+                                            <button onClick={() => setCurrentIndex(index)} className='w-full text-left p-2 py-3 mb-2 shadow-sm rounded-lg font-semibold bg-white border border-blue-100 text-gray-800 hover:shadow-md hover:border-emerald-400 ease-in-out duration-300 '>{item?.name}</button>
                                         </div>
                                     ))
                                 }
